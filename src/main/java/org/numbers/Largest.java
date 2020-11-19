025490a474932f3614bd45b2c0b29ff89c10e218
Swopnalika <swopnalikamohapatra185@gmail.com>
2020-11-19 22:12:48 +0530

    largest of 2 nos.

---
+++ src/main/java/org/numbers/Largest.java
@@ -0,0 +1,25 @@
+package org.numbers;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        //scanner class declaration
+        Scanner sc = new Scanner(System.in);
+        //input first number
+        System.out.print("Enter the first number : ");
+        int first = sc.nextInt();
+        //input second number
+        System.out.print("Enter the second number : ");
+        int second = sc.nextInt();
+        //conditions
+        if (first > second)
+            System.out.println(first + " is greater than " + second);
+        else if (second > first)
+            System.out.println(second + " is greater than " + first);
+        else
+            System.out.println("Both numbers are Equal");
+
+
+    }
+}
