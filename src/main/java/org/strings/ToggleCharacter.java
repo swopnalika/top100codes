b38a6a5d102ed8828f4ab203ee82da4db06b79a6
Swopnalika <swopnalikamohapatra185@gmail.com>
2020-11-21 21:52:16 +0530

    strings

---
+++ src/main/java/org/strings/ToggleCharacter.java
@@ -0,0 +1,22 @@
+package org.strings;
+
+import java.util.Scanner;
+
+public class ToggleCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String : ");
+        String s = sc.nextLine();
+        String s1 = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i)))
+                s1 = s1 + Character.toLowerCase(s.charAt(i));
+            else
+                s1 = s1 + Character.toUpperCase(s.charAt(i));
+        }
+        System.out.println("Toggle String is : " + s1);
+
+    }
+
+}
+
