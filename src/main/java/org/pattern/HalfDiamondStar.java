100789aaea4a0300d65f62e156252abae146812c
Swopnalika <swopnalikamohapatra185@gmail.com>
2020-11-23 19:10:52 +0530

    pattern prog added

---
+++ src/main/java/org/pattern/HalfDiamondStar.java
@@ -0,0 +1,23 @@
+package org.pattern;
+
+import java.util.Scanner;
+
+public class HalfDiamondStar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no");
+        int no = sc.nextInt();
+
+        for (int i = 1; i <= no; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+        for (int i = no - 1; i > 0; i--) {
+            for (int j = 1; j <= i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
